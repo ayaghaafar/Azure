52163504adf71bc75c0fd50cc803ae4467ba5cea
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-04 15:58:38 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ sudo apt-get install unzip
 wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
 unzip jenkinsfile-runner-1.0-beta-30.zip -d jenkinsfile-runner
 chmod -R 777 jenkinsfile-runner
-./jenkinsfile-runner/bin/jenkinsfile-runner cli -p https://updates.jenkins.io/download/plugins/durable-task/1.35/durable-task.hpi
+sudo java -jar /home/jenkins/jenkins-cli.jar -s http://localhost:8080 install-plugin http://updates.jenkins-ci.org/download/plugins/git/latest/git.hpi
 ./jenkinsfile-runner/bin/jenkinsfile-runner run
