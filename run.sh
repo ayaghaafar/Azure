e2e2c764e9ab0d427220716b01a26fd97bb00973
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-04 16:54:40 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-
 unzip jenkinsfile-runner-1.0-beta-30.zip -d jenkinsfile-runner
 chmod -R 777 jenkinsfile-runner
 wget -q https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/plugin-management-parent-pom-2.0.0/jenkins-plugin-manager-2.0.0.jar -O /usr/local/lib/jenkins-plugin-manager.jar
+chmod -R 777 /usr/local/lib/jenkins-plugin-manager.jar
 java -jar /usr/local/lib/jenkins-plugin-manager.jar -f /usr/share/jenkins/ref/plugins.txt
 # sudo java -jar /home/jenkins/jenkins-cli.jar -s http://localhost:8080 install-plugin http://updates.jenkins-ci.org/download/plugins/git/latest/git.hpi
 ./jenkinsfile-runner/bin/jenkinsfile-runner run
