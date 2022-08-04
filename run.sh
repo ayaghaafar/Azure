700534a23c7d0e71a7d0a3a4395294d0dd9fc9d2
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-04 17:02:44 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,11 @@ sudo apt-get install unzip
 wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
 unzip jenkinsfile-runner-1.0-beta-30.zip -d jenkinsfile-runner
 chmod -R 777 jenkinsfile-runner
-wget -q https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/plugin-management-parent-pom-2.0.0/jenkins-plugin-manager-2.0.0.jar -O jenkins-plugin-manager.jar
-chmod 777 jenkins-plugin-manager.jar
-java -jar jenkins-plugin-manager.jar
+sudo service jenkins stop
+sudo mv jenkins.war jenkins.war.old
+sudo wget https://updates.jenkins-ci.org/latest/jenkins.war
+# wget -q https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/plugin-management-parent-pom-2.0.0/jenkins-plugin-manager-2.0.0.jar -O jenkins-plugin-manager.jar
+# chmod 777 jenkins-plugin-manager.jar
+# java -jar jenkins-plugin-manager.jar
 # sudo java -jar /home/jenkins/jenkins-cli.jar -s http://localhost:8080 install-plugin http://updates.jenkins-ci.org/download/plugins/git/latest/git.hpi
-#./jenkinsfile-runner/bin/jenkinsfile-runner run
+./jenkinsfile-runner/bin/jenkinsfile-runner run
