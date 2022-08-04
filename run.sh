4158608f5754c534a95606dbf079796b1536a822
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-04 15:01:07 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 sudo apt-get install groovy -y
 sudo apt-get install unzip
 wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
-unzip jenkinsfile-runner-1.0-beta-30.zip
-./jenkinsfile-runner-1.0-beta-30/bin/jenkinsfile-runner -w /tmp/jenkins -p /tmp/jenkins_home/plugins -f . -a "param1=Hello&param2=value2"
+unzip jenkinsfile-runner-1.0-beta-30.zip -d jenkinsfile-runner
+./jenkinsfile-runner/bin/jenkinsfile-runner -w /tmp/jenkins -p /tmp/jenkins_home/plugins -f . -a "param1=Hello&param2=value2"
 # chmod 777 Jenkinsfile.groovy
 # groovy Jenkinsfile.groovy
