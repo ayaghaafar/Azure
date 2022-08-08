a910e4a06dbe3e72e53909d1dd51ef3b7997e04a
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-08 16:50:33 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,11 @@ sudo apt-get install unzip
 wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
 unzip jenkinsfile-runner-1.0-beta-30.zip -d jenkinsfile-runner
 chmod -R 777 jenkinsfile-runner
+jenkins-plugin-cli --plugins workflow-aggregator filesystem_scm git
+cd /usr/share/jenkins && jar -xvf jenkins.war
 export JENKINS_HOME="/usr/share/jenkins/ref/"
 export JAVA_OPTS="-Djenkins.model.Jenkins.slaveAgentPort=50000 -Djenkins.model.Jenkins.slaveAgentPortEnforce=true -Dhudson.model.LoadStatistics.clock=1000"
-#sudo wget https://updates.jenkins-ci.org/latest/jenkins.war
-./jenkinsfile-runner/bin/jenkinsfile-runner run
+./jenkinsfile-runner/bin/jenkinsfile-runner -w /usr/share/jenkins/ -p /usr/share/jenkins/ref/plugins -f
 # wget -q https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/plugin-management-parent-pom-2.0.0/jenkins-plugin-manager-2.0.0.jar -O jenkins-plugin-manager.jar
 # chmod 777 jenkins-plugin-manager.jar
 # java -jar jenkins-plugin-manager.jar
