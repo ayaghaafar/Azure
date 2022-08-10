4a8137b28994c7c741186300c6fdf62739fccd7c
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-10 13:17:56 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 sudo apt-get install groovy -y
 sudo apt-get install unzip
+sudo apt-get install openjdk-11-jre
 wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
 unzip jenkinsfile-runner-1.0-beta-30.zip -d jenkinsfile-runner
 chmod -R 777 jenkinsfile-runner
