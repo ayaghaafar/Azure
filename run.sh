c1bde26a9ba48ab0cdd3a378b35e608ac256288f
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-10 13:55:01 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 sudo apt-get install groovy -y
 sudo apt-get install unzip
 sudo apt-get install openjdk-11-jre
-wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
-unzip jenkinsfile-runner-1.0-beta-30.zip -d jenkinsfile-runner
-chmod -R 777 jenkinsfile-runner
-export JAVA_OPTS="-Djenkins.model.Jenkins.slaveAgentPort=50000 -Djenkins.model.Jenkins.slaveAgentPortEnforce=true -Dhudson.model.LoadStatistics.clock=1000"
-./jenkinsfile-runner/bin/jenkinsfile-runner run
+wget https://github.com/jenkinsci/configuration-as-code-groovy-plugin
+# wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
+# unzip jenkinsfile-runner-1.0-beta-30.zip -d jenkinsfile-runner
+# chmod -R 777 jenkinsfile-runner
+# export JAVA_OPTS="-Djenkins.model.Jenkins.slaveAgentPort=50000 -Djenkins.model.Jenkins.slaveAgentPortEnforce=true -Dhudson.model.LoadStatistics.clock=1000"
+# ./jenkinsfile-runner/bin/jenkinsfile-runner run
