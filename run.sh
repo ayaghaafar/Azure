280d3b3538bef0ae4f9869e8d0de1c656c7357dc
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-10 14:24:11 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 sudo apt-get install groovy -y
 sudo apt-get install unzip
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian/jenkins.io.key
+sudo apt-get update -qq
 sudo apt-get install Jenkins
 sudo apt-get install Jenkins-lts
-sudo service start Jenkins-lts
+sudo service start Jenkins
 # jenkins-plugin-cli --plugins configuration-as-code-groovy:1.1
 Java – jar jenkins-cli.jar –s http://localhost:8080/ enable-plugin configuration-as-code-groovy:1.1
 jar jenkins-cli.jar  -s http://localhost:8080/ -auth admin:password groovy = < Jenkinsfile
