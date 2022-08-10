f06e8441a833f17a3cfa005503a08e6d29c4abba
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-10 12:46:48 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-
 unzip jenkinsfile-runner-1.0-beta-30.zip -d jenkinsfile-runner
 chmod -R 777 jenkinsfile-runner
 export JAVA_OPTS="-Djenkins.model.Jenkins.slaveAgentPort=50000 -Djenkins.model.Jenkins.slaveAgentPortEnforce=true -Dhudson.model.LoadStatistics.clock=1000"
-jar -xvf /home/vsts/.jenkinsfile-runner/war/2.346.2/jenkins-war-2.346.2.war
 ./jenkinsfile-runner/bin/jenkinsfile-runner -w /usr/share/jenkins/ -p /usr/share/jenkins/ref/plugins -f
 # wget -q https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/plugin-management-parent-pom-2.0.0/jenkins-plugin-manager-2.0.0.jar -O jenkins-plugin-manager.jar
 # chmod 777 jenkins-plugin-manager.jar
