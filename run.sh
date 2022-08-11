15d592e60fad8edcd223fbc9f29b4b9e674dc479
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-11 16:12:29 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ sudo apt-get install unzip
 wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
 unzip jenkinsfile-runner-1.0-beta-30.zip -d jenkinsfile-runner
 chmod -R 777 jenkinsfile-runner
+wget https://get.jenkins.io/war-stable/latest/jenkins.war
 export JAVA_OPTS="-Djenkins.model.Jenkins.slaveAgentPort=50000 -Djenkins.model.Jenkins.slaveAgentPortEnforce=true -Dhudson.model.LoadStatistics.clock=1000"
-./jenkinsfile-runner/bin/jenkinsfile-runner run
+./jenkinsfile-runner/bin/jenkinsfile-runner -w /home/builder/Workspaces/jfRunnerDemo/jenkins.war -p /home/builder/Workspaces/jfRunnerDemo/plugins -f
