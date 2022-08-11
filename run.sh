fafbb122197cd204332ac8ecd34271d6e293ddaa
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-12 00:37:43 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 sudo apt-get install groovy -y
 sudo apt-get install unzip
 sudo apt install default-jre
+sudo apt-get install git
 java -version
 sudo apt install default-jdk
 javac -version
-wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
+git pull https://github.com/ayaghaafar/Jenkins-runner.git
+# wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
 #wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-29/jenkinsfile-runner-1.0-beta-29.zip
 # unzip jenkinsfile-runner-1.0-beta-29.zip
-unzip jenkinsfile-runner-1.0-beta-30.zip -d /usr/share
+# unzip jenkinsfile-runner-1.0-beta-30.zip -d /usr/share
+cp -R Jenkins-runner /usr/share/
 wget https://get.jenkins.io/war-stable/2.277.1/jenkins.war -P /usr/share/lib
 chmod -R 777 /usr/share/bin/jenkinsfile-runner
-/usr/share/bin/jenkinsfile-runner -p plugins.txt -f Jenkinsfile
+/usr/share/Jenkins-runner/bin/jenkinsfile-runner -p plugins.txt -f Jenkinsfile
