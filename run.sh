a45b8058cee2ac2431d8589f3686b67a891113ea
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-11 16:51:27 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 sudo apt-get install groovy -y
 sudo apt-get install unzip
-wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
-unzip jenkinsfile-runner-1.0-beta-30.zip -d jenkinsfile-runner
-chmod -R 777 jenkinsfile-runner
-export JAVA_OPTS="-Djenkins.model.Jenkins.slaveAgentPort=50000 -Djenkins.model.Jenkins.slaveAgentPortEnforce=true -Dhudson.model.LoadStatistics.clock=1000"
-sudo apt-get install default-jdk
-sudo apt-get install default-jre
-./jenkinsfile-runner/bin/jenkinsfile-runner -p plugins.txt -f Jenkinsfile
+# wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
+wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-29/jenkinsfile-runner-1.0-beta-29.zip
+unzip jenkinsfile-runner-1.0-beta-29.zip
+# unzip jenkinsfile-runner-1.0-beta-30.zip -d jenkinsfile-runner
+# chmod -R 777 jenkinsfile-runner
+# export JAVA_OPTS="-Djenkins.model.Jenkins.slaveAgentPort=50000 -Djenkins.model.Jenkins.slaveAgentPortEnforce=true -Dhudson.model.LoadStatistics.clock=1000"
+# sudo apt-get install default-jdk
+# sudo apt-get install default-jre
+./bin/jenkinsfile-runner -p plugins.txt -f Jenkinsfile
