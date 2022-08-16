57c4f9d3ab4cbaff3f589a00e79bd36dc00a63a7
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-16 11:30:25 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,12 +5,7 @@ sudo apt-get install git
 java -version
 sudo apt install default-jdk
 javac -version
-git clone https://github.com/ayaghaafar/Jenkins-runner.git
-# wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
-#wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-29/jenkinsfile-runner-1.0-beta-29.zip
-# unzip jenkinsfile-runner-1.0-beta-29.zip
-# unzip jenkinsfile-runner-1.0-beta-30.zip -d /usr/share
-cp -R Jenkins-runner /usr/share/
-wget https://get.jenkins.io/war-stable/2.277.1/jenkins.war -P /usr/share/lib
-chmod -R 777 /usr/share/Jenkins-runner/bin/jenkinsfile-runner
-/usr/share/Jenkins-runner/bin/jenkinsfile-runner -p plugins.txt -f Jenkinsfile
+wget jenkinsfile-runner-1.0-beta-30.zip
+unzip jenkinsfile-runner-1.0-beta-30.zip
+chmod -R 777 ./*
+./bin/jenkinsfile-runner -p plugins.txt -f helloworld/jenkinsfile
