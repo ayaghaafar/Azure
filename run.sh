4ad5ff18c857cd741ac6efd70039d5e90a37b810
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-16 11:35:20 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ sudo apt-get install git
 java -version
 sudo apt install default-jdk
 javac -version
-wget jenkinsfile-runner-1.0-beta-30.zip
+wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
 unzip jenkinsfile-runner-1.0-beta-30.zip
-chmod -R 777 ./*
+chmod -R 777 *
 ./bin/jenkinsfile-runner -p plugins.txt -f helloworld/jenkinsfile
