7c9f608fc7f12901aa74158a866ffa66ce5b26bd
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-17 13:17:37 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -22,4 +22,5 @@ wget https://github.com/jenkinsci/plugin-installation-manager-tool/releases/down
 cd plugin-installation-manager-tool-master/
 mvn clean install
 java -jar plugin-management-cli/target/jenkins-plugin-manager-*.jar --war ../jenkins.war --plugin-file ../plugins.txt -d plugins --plugins delivery-pipeline-plugin:1.3.2 deployit-plugin
+ls
 #./bin/jenkinsfile-runner -f ../Jenkinsfile
