48b6a10116a03a474bf1a49da34fe907649a4419
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-17 14:56:13 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 sudo apt-get install groovy -y
 sudo apt-get install unzip
-sudo apt install default-jre
 sudo apt-get install git
-java -version
-sudo apt install default-jdk
 wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
 unzip jenkinsfile-runner-1.0-beta-30.zip
 wget https://updates.jenkins.io/download/war/2.363/jenkins.war
@@ -20,9 +17,9 @@ wget https://github.com/jenkinsci/plugin-installation-manager-tool/archive/refs/
 unzip master.zip
 wget https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/2.12.8/jenkins-plugin-manager-2.12.8.jar
 cd plugin-installation-manager-tool-master/
-mvn clean install
+mvn install
 java -jar plugin-management-cli/target/jenkins-plugin-manager-*.jar --war ../jenkins.war --plugin-file ../plugins.txt -d plugins --plugins delivery-pipeline-plugin:1.3.2 deployit-plugin
-ls
+ls target
 cd ../
 ps
 ./bin/jenkinsfile-runner -w jenkins.war -p plugin-installation-manager-tool-master/plugins -f Jenkinsfile
