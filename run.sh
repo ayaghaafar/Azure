5437d93e22e7f5f86ecfaf24477d43678eba7d9c
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-17 15:56:05 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@ sudo apt-get install groovy -y
 sudo apt-get install unzip
 sudo apt-get install git
 sudo apt install jetty9
-sudo systemctl start jetty9
 wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
 unzip jenkinsfile-runner-1.0-beta-30.zip
 wget https://updates.jenkins.io/download/war/2.363/jenkins.war
@@ -19,10 +18,11 @@ wget https://github.com/jenkinsci/plugin-installation-manager-tool/archive/refs/
 unzip master.zip
 wget https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/2.12.8/jenkins-plugin-manager-2.12.8.jar
 cd plugin-installation-manager-tool-master/
-mvn install
+#mvn install
 java -jar plugin-management-cli/target/jenkins-plugin-manager-*.jar --war ../jenkins.war --plugin-file ../plugins.txt -d plugins --plugins delivery-pipeline-plugin:1.3.2 deployit-plugin
 ls target
 cd ../
 sudo systemctl start jetty9
-ps
-./bin/jenkinsfile-runner -w jenkins.war -p plugin-installation-manager-tool-master/plugins -f Jenkinsfile
+sudo systemctl status jetty9
+netstat -nltp
+./bin/jenkinsfile-runner -w jenkins.war --httpPort=9090 -p plugin-installation-manager-tool-master/plugins -f Jenkinsfile
