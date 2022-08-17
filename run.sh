d9bc29b4fec40e2e21953d0b70849ddde77723c5
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-17 15:21:05 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ sudo apt-get install groovy -y
 sudo apt-get install unzip
 sudo apt-get install git
 sudo apt install jetty9
+sudo systemctl start jetty9
 wget https://github.com/jenkinsci/jenkinsfile-runner/releases/download/1.0-beta-30/jenkinsfile-runner-1.0-beta-30.zip
 unzip jenkinsfile-runner-1.0-beta-30.zip
 wget https://updates.jenkins.io/download/war/2.363/jenkins.war
