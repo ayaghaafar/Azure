b03ace4eacd3edf9ee6badf60c32593827d8b57e
ayaghaafar <85257960+ayaghaafar@users.noreply.github.com>
2022-08-17 12:23:37 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,6 @@ unzip jenkinsfile-runner-1.0-beta-30.zip
 wget https://updates.jenkins.io/download/war/2.363/jenkins.war
 #wget https://get.jenkins.io/war-stable/latest/jenkins.war
 chmod -R 777 *
-mvn clean install
 wget https://download.java.net/java/GA/jdk13.0.1/cec27d702aa74d5a8630c65ae61e4305/9/GPL/openjdk-13.0.1_linux-x64_bin.tar.gz
 tar -xvf openjdk-13.0.1_linux-x64_bin.tar.gz
 mv jdk-13.0.1 /opt/
@@ -20,5 +19,6 @@ java -version
 wget https://github.com/jenkinsci/plugin-installation-manager-tool/archive/refs/heads/master.zip
 unzip master.zip
 wget https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/2.12.8/jenkins-plugin-manager-2.12.8.jar
-java -jar jenkins-plugin-manager-2.12.8.jar --war jenkins.war --plugin-file plugins.txt --plugins delivery-pipeline-plugin:1.3.2 deployit-plugin
-./bin/jenkinsfile-runner -w jenkins.war -p plugins.txt -f Jenkinsfile
+mvn clean install
+java -jar jenkins-plugin-manager-2.12.8.jar --war jenkins.war --plugin-file plugins.txt -d plugins --plugins delivery-pipeline-plugin:1.3.2 deployit-plugin
+#./bin/jenkinsfile-runner -w jenkins.war -p plugins.txt -f Jenkinsfile
